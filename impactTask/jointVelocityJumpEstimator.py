42fedb9103560423db386ecc7bdb6a956398677b
yuquan <yuquan@kth.se>
2018-11-25 03:26:24 +0100

    add the bounds on joint acceleration at impact induced by joint position and velocity limits

--- impactTask/jointVelocityJumpEstimator.py
+++ impactTask/jointVelocityJumpEstimator.py
@@ -24,23 +24,42 @@ class jointVelocityJumpEstimator:
         self.resLower = resLower
         self.resUpper = resUpper
 
+        self.positionUpper = (self.robot.position_upper_limits()).reshape((self.robot.ndofs, 1))
+        self.positionLower = (self.robot.position_lower_limits()).reshape((self.robot.ndofs, 1))
+
+        self.velocityUpper = []
+        self.velocityLower = []
+        for ii in range(0, self.robot.ndofs):
+            dof = self.robot.dof(ii)
+            self.velocityUpper.append(dof.velocity_upper_limit())
+            self.velocityLower.append(dof.velocity_lower_limit())
+
+        self.velocityUpper = np.reshape(self.velocityUpper, (self.robot.ndofs, 1))
+        self.velocityLower = np.reshape(self.velocityLower, (self.robot.ndofs, 1))
+
         self.dt = self.robot.world.dt
 
     def initializeLog(self):
 
-        self.predictionLog = namedtuple('log', ['delta_dq_lower','delta_dq_upper'])
+        self.predictionLog = namedtuple('log', ['delta_dq_lower','delta_dq_upper', 'ddqUpperBoundPosition', 'ddqLowerBoundPosition','ddqUpperBoundVelocity', 'ddqLowerBoundVelocity'])
         self.time = []
         self.predictionLog.deltaDqLower = []
         self.predictionLog.deltaDqUpper = []
+        self.predictionLog.ddqUpperBoundPosition = []
+        self.predictionLog.ddqLowerBoundPosition = []
+        self.predictionLog.ddqUpperBoundVelocity = []
+        self.predictionLog.ddqLowerBoundVelocity = []
 
-    def saveLog(self, predictDeltaDqLower, predictDeltaDqUpper):
+    def saveLog(self, predictDeltaDqLower, predictDeltaDqUpper,ddqLowerPosition, ddqUpperPosition, ddqLowerVelocity, ddqUpperVelocity):
 
         self.time.append(self.robot.world.t)
 
         self.predictionLog.deltaDqLower.append(predictDeltaDqLower)
         self.predictionLog.deltaDqUpper.append(predictDeltaDqUpper)
-
-
+        self.predictionLog.ddqUpperBoundPosition.append(ddqUpperPosition)
+        self.predictionLog.ddqLowerBoundPosition.append(ddqLowerPosition)
+        self.predictionLog.ddqUpperBoundVelocity.append(ddqUpperVelocity)
+        self.predictionLog.ddqLowerBoundVelocity.append(ddqLowerVelocity)
 
     def calcImpulsiveQuantities(self, sol_ddq):
 
@@ -62,13 +81,24 @@ class jointVelocityJumpEstimator:
         predicted_delta_dq_upper = self.resUpper*baseOne
         predicted_delta_dq_lower = self.resLower*baseOne
 
-        return [predicted_delta_dq_lower, predicted_delta_dq_upper]
+        ddq = (self.robot.ddq).reshape((self.robot.ndofs, 1))
+        position = (self.robot.q).reshape((self.robot.ndofs, 1))
+
+
+        ddq_upper_bound_position = (self.positionUpper - position + ddq*(self.dt*self.dt) + predicted_delta_dq_upper*self.dt)/(self.dt*self.dt)
+        ddq_lower_bound_position = (self.positionLower - position + ddq * (self.dt*self.dt) + predicted_delta_dq_upper * self.dt) / (
+        self.dt*self.dt)
+
+        ddq_upper_bound_Velocity = -(self.velocityLower - dq - ddq * self.dt - predicted_delta_dq_upper) / self.dt
+        ddq_lower_bound_Velocity = -(self.velocityUpper - dq - ddq*self.dt - predicted_delta_dq_upper)/self.dt
+
+        return [predicted_delta_dq_lower, predicted_delta_dq_upper, ddq_lower_bound_position, ddq_upper_bound_position, ddq_lower_bound_Velocity, ddq_upper_bound_Velocity]
 
 
     def update(self, sol_ddq):
 
-        [predicted_delta_dq_lower, predicted_delta_dq_upper] = self.calcImpulsiveQuantities(sol_ddq)
+        [predicted_delta_dq_lower, predicted_delta_dq_upper, ddq_lower_bound_position, ddq_upper_bound_position, ddq_lower_bound_Velocity, ddq_upper_bound_Velocity] = self.calcImpulsiveQuantities(sol_ddq)
 
 
-        self.saveLog(predicted_delta_dq_lower, predicted_delta_dq_upper)
+        self.saveLog(predicted_delta_dq_lower, predicted_delta_dq_upper, ddq_lower_bound_position, ddq_upper_bound_position, ddq_lower_bound_Velocity, ddq_upper_bound_Velocity)
 
--- utils/impact_plotter_three.py
+++ utils/impact_plotter_three.py
@@ -38,8 +38,33 @@ if __name__ =="__main__":
     predict_delta_dq_lower_5 = loaded['predict_jointVelocityJump_lower'][:,5]
 
 
+    ddq_upper_bound_position_0 = loaded['ddqUpperBoundPosition'][:,0]
+    ddq_upper_bound_position_1 = loaded['ddqUpperBoundPosition'][:,1]
+    ddq_upper_bound_position_2 = loaded['ddqUpperBoundPosition'][:,2]
+    ddq_upper_bound_position_3 = loaded['ddqUpperBoundPosition'][:,3]
+    ddq_upper_bound_position_4 = loaded['ddqUpperBoundPosition'][:,4]
+    ddq_upper_bound_position_5 = loaded['ddqUpperBoundPosition'][:,5]
 
+    ddq_lower_bound_position_0 = loaded['ddqLowerBoundPosition'][:,0]
+    ddq_lower_bound_position_1 = loaded['ddqLowerBoundPosition'][:,1]
+    ddq_lower_bound_position_2 = loaded['ddqLowerBoundPosition'][:,2]
+    ddq_lower_bound_position_3 = loaded['ddqLowerBoundPosition'][:,3]
+    ddq_lower_bound_position_4 = loaded['ddqLowerBoundPosition'][:,4]
+    ddq_lower_bound_position_5 = loaded['ddqLowerBoundPosition'][:,5]
 
+    ddq_upper_bound_velocity_0 = loaded['ddqUpperBoundVelocity'][:,0]
+    ddq_upper_bound_velocity_1 = loaded['ddqUpperBoundVelocity'][:,1]
+    ddq_upper_bound_velocity_2 = loaded['ddqUpperBoundVelocity'][:,2]
+    ddq_upper_bound_velocity_3 = loaded['ddqUpperBoundVelocity'][:,3]
+    ddq_upper_bound_velocity_4 = loaded['ddqUpperBoundVelocity'][:,4]
+    ddq_upper_bound_velocity_5 = loaded['ddqUpperBoundVelocity'][:,5]
+
+    ddq_lower_bound_velocity_0 = loaded['ddqLowerBoundVelocity'][:,0]
+    ddq_lower_bound_velocity_1 = loaded['ddqLowerBoundVelocity'][:,1]
+    ddq_lower_bound_velocity_2 = loaded['ddqLowerBoundVelocity'][:,2]
+    ddq_lower_bound_velocity_3 = loaded['ddqLowerBoundVelocity'][:,3]
+    ddq_lower_bound_velocity_4 = loaded['ddqLowerBoundVelocity'][:,4]
+    ddq_lower_bound_velocity_5 = loaded['ddqLowerBoundVelocity'][:,5]
 
     fig2, (ax21, ax22, ax23, ax24, ax25, ax26) = plt.subplots(nrows=6, ncols=1)
 
@@ -110,4 +135,135 @@ if __name__ =="__main__":
     ax26.autoscale(enable=True, axis='x', tight=True)
     fig2.savefig("Delta_dq_bounds.pdf", bbox_inches='tight')
     
+
+
+
+
+
+
+
+
+    fig3, (ax31, ax32, ax33, ax34, ax35, ax36) = plt.subplots(nrows=6, ncols=1)
+
+    ax31 = plt.subplot(611)
+    ax31.set_ylabel('ddq 0')
+    ax31.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    plt.plot(time, ddq_upper_bound_position_0, label='Upper bound: Position')
+    plt.plot(time, ddq_lower_bound_position_0, label='Lower bound: Position')
+
+    ax31.locator_params(nbins=6, axis='y')
+    ax31.autoscale(enable=True, axis='x', tight=True)
+    plt.setp(ax31.get_xticklabels(), visible=False)
+    plt.grid(True)
+    ax31.legend(frameon=False, loc='upper left', prop=fontP)
+    plt.title("Bounds on joint accelerations under impacts [Radian/Second^2]")
+
+    ax32 = plt.subplot(612)
+    plt.plot(time, ddq_upper_bound_position_1)
+    plt.plot(time, ddq_lower_bound_position_1)
+    ax32.set_ylabel('ddq 1')
+    ax32.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    ax32.autoscale(enable=True, axis='x', tight=True)
+    plt.setp(ax32.get_xticklabels(), visible=False)
+    plt.grid(True)
+
+    ax33 = plt.subplot(613)
+    plt.plot(time, ddq_upper_bound_position_2)
+    plt.plot(time, ddq_lower_bound_position_2)
+    ax33.set_ylabel('ddq 2')
+    ax33.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    ax33.autoscale(enable=True, axis='x', tight=True)
+    plt.setp(ax33.get_xticklabels(), visible=False)
+    plt.grid(True)
+
+    ax34 = plt.subplot(614)
+    plt.plot(time, ddq_upper_bound_position_3)
+    plt.plot(time, ddq_lower_bound_position_3)
+    ax34.set_ylabel('ddq 3')
+    ax34.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    ax34.autoscale(enable=True, axis='x', tight=True)
+    plt.setp(ax34.get_xticklabels(), visible=False)
+    plt.grid(True)
+
+    ax35 = plt.subplot(615)
+    plt.plot(time, ddq_upper_bound_position_4)
+    plt.plot(time, ddq_lower_bound_position_4)
+    ax35.set_ylabel('ddq 4')
+    ax35.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+    ax35.autoscale(enable=True, axis='x', tight=True)
+    plt.setp(ax35.get_xticklabels(), visible=False)
+    plt.grid(True)
+
+    ax36 = plt.subplot(616)
+    plt.plot(time, ddq_upper_bound_position_5)
+    plt.plot(time, ddq_lower_bound_position_5)
+    ax36.set_ylabel('ddq 5')
+    plt.xlabel('Time [s]')
+    ax36.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    ax36.autoscale(enable=True, axis='x', tight=True)
+    plt.grid(True)
+    fig3.savefig("ddq_position_impact_bounds.pdf", bbox_inches='tight')
+
+    
+    
+    fig4, (ax41, ax42, ax43, ax44, ax45, ax46) = plt.subplots(nrows=6, ncols=1)
+    ax41 = plt.subplot(611)
+    ax41.set_ylabel('ddq 0')
+    ax41.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    plt.plot(time, ddq_upper_bound_velocity_0, label='Upper bound: Velocity')
+    plt.plot(time, ddq_lower_bound_velocity_0, label='Lower bound: Velocity')
+
+    ax41.locator_params(nbins=6, axis='y')
+    ax41.autoscale(enable=True, axis='x', tight=True)
+    plt.setp(ax41.get_xticklabels(), visible=False)
+    plt.grid(True)
+    ax41.legend(frameon=False, loc='upper left', prop=fontP)
+    plt.title("Bounds on joint accelerations under impacts [Radian/Second^2]")
+
+    ax42 = plt.subplot(612)
+    plt.plot(time, ddq_upper_bound_velocity_1)
+    plt.plot(time, ddq_lower_bound_velocity_1)
+    ax42.set_ylabel('ddq 1')
+    ax42.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    ax42.autoscale(enable=True, axis='x', tight=True)
+    plt.setp(ax42.get_xticklabels(), visible=False)
+    plt.grid(True)
+
+    ax43 = plt.subplot(613)
+    plt.plot(time, ddq_upper_bound_velocity_2)
+    plt.plot(time, ddq_lower_bound_velocity_2)
+    ax43.set_ylabel('ddq 2')
+    ax43.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    ax43.autoscale(enable=True, axis='x', tight=True)
+    plt.setp(ax43.get_xticklabels(), visible=False)
+    plt.grid(True)
+    
+    ax44 = plt.subplot(614)
+    plt.plot(time, ddq_upper_bound_velocity_3)
+    plt.plot(time, ddq_lower_bound_velocity_3)
+    ax44.set_ylabel('ddq 3')
+    ax44.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    ax44.autoscale(enable=True, axis='x', tight=True)
+    plt.setp(ax44.get_xticklabels(), visible=False)
+    plt.grid(True)
+    
+    ax45 = plt.subplot(615)
+    plt.plot(time, ddq_upper_bound_velocity_4)
+    plt.plot(time, ddq_lower_bound_velocity_4)
+    ax45.set_ylabel('ddq 4')
+    ax45.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    ax45.autoscale(enable=True, axis='x', tight=True)
+    plt.setp(ax45.get_xticklabels(), visible=False)
+    plt.grid(True)
+
+    ax46 = plt.subplot(616)
+    plt.plot(time, ddq_upper_bound_velocity_5)
+    plt.plot(time, ddq_lower_bound_velocity_5)
+    ax46.set_ylabel('ddq 5')
+    plt.xlabel('Time [s]')
+    ax46.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    ax46.autoscale(enable=True, axis='x', tight=True)
+    plt.grid(True)
+    fig4.savefig("ddq_velocity_impact_bounds.pdf", bbox_inches='tight')
+
     plt.show()
